68f259f13c78d7b946fddfa6363d773df06f9687
adrianromanski <adrianromanski@icloud.com>
2020-11-10 21:07:35 +0100

    Added Gallery Section - PhotoX

--- photoX/script.js
+++ photoX/script.js
@@ -5,27 +5,42 @@ $(document).ready(function() {
    });
 
 
-//navigation
-$(window).scroll(function () {
-    let position = $(this).scrollTop();
-    if(position >= 200) {
-        $('.nav-menu').addClass('custom-navbar');
-    } else {
-        $('.nav-menu').removeClass('custom-navbar');
-    }
-});
+    //navigation
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if(position >= 200) {
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+    });
 
 
-//mission section
-$(window).scroll(function () {
-    let position = $(this).scrollTop();
-    if(position >= 750) {
-        $('.camera-img').addClass('fromLeft');
-        $('.mission-text').addClass('fromRight');
-    } else {
-        $('.camera-img').removeClass('fromLeft');
-        $('.mission-text').removeClass('fromRight');
-    }
-})
+    //mission section
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if(position >= 750) {
+            $('.camera-img').addClass('fromLeft');
+            $('.mission-text').addClass('fromRight');
+        } else {
+            $('.camera-img').removeClass('fromLeft');
+            $('.mission-text').removeClass('fromRight');
+        }
+    });
+
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'all') {
+            $('.filter').show(300);
+        } else {
+            $('.filter').not('.' + value).hide(300);
+            $('.filter').filter('.' + value).show(300);
+        }
+    });
+
+    $('.gallery-list-item').click(function() {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
+
 
 });
