2b317bde317e67706db6a4894c4062f7936e3590
adrianromanski <adrianromanski@icloud.com>
2020-11-20 12:03:33 +0100

    Added Famous Writers - Book Store

--- book_store/js/script.js
+++ book_store/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     lightbox.option({
         'wrapAround': true
-    })
+    });
 
     $(window).scroll(function () {
         let position = $(this).scrollTop();
@@ -11,9 +11,18 @@ $(document).ready(function () {
         } else {
             $('.gallery').removeClass('change');
         }
-    })
+    });
 
-})
+    $('.writers-accordion').click(function(event) {
+
+        if(event.target.id.split('-')[0] === 'button') {
+            $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg')
+            $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg')
+        }
+
+    });
+
+});
 
 
 
